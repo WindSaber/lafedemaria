e0e07492263d95debfc96ec5e29af67548780959
Omar Nava <ing.omarnavap@hotmail.com>
2015-11-24 09:19:04 -0600

    Auth
    alter table integrante modify column password varchar(60);
    alter table integrante add column remember_token varchar(100);

--- app/Models/Integrante.php
+++ app/Models/Integrante.php
@@ -1,7 +1,16 @@
 <?php
 namespace App\Models;
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
-class Integrante extends Model{
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+class Integrante extends Model implements AuthenticatableContract,
+                                    AuthorizableContract,
+                                    CanResetPasswordContract{
+    use Authenticatable, Authorizable, CanResetPassword;
     public $table = 'integrante';
     public $fillable = ['datos_personales_id','rol_id','ubicacion_responsable','celular',
         'telefono','facebook','password'];
@@ -14,4 +23,5 @@ class Integrante extends Model{
     public function rol()    {
         return $this->belongsTo('App\Models\Rol');
     }
+
 }
